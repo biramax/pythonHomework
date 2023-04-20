ad76a99735089f064603a826e2b346506f6cf730
Biramax <biramax@biramax>
2023-04-20 19:38:37 +0300

    Task 2 done

--- task7.2.py
+++ task7.2.py
@@ -17,10 +17,8 @@ def print_operation_table(operation, num_rows = 6, num_columns = 6):
     for i in range(1, num_rows + 1):
         print(i, end = ' ')
         for j in range(2, num_columns + 1):
-            if i == 1:
-                print(j, end = ' ')
-            else:
-                print(operation(i, j), end = ' ')
-        print('')
+            print(operation(i, j), end = ' ')
+        print()
 
 print_operation_table(lambda x, y: x * y)
+
